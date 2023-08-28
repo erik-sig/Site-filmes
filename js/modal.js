d07f6defd2ca7d4cebbcf6189e8dc3717dd66aa4
Erik Signori Thalheimer <erik.signori_thalheimer@hotmail.com>
2023-08-28 19:11:19 -0300

    First commit with archives

---
+++ js/modal.js
@@ -0,0 +1,39 @@
+const background = document.getElementById("modal-background");
+const modelContainer = document.getElementById("modal-container");
+
+function backgroundClickHandler() {
+  overlay.classList.remove("open");
+}
+
+function createModal(data) {
+  modelContainer.innerHTML = `
+  <section id="modal-container">
+                <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
+                <section id="modal-body">
+                    <img id="movie-poster"
+                        src=${data.Poster}
+                        alt="Poster do Filme">
+                    <div id="movie-info">
+                        <h3 id="movie-plot">
+                        ${data.Plot}
+                        </h3>
+                        <div id="movie-cast">
+                            <h4>
+                                ${data.Actors}
+                            </h4>
+                        </div>
+                        <div id="movie-genre">
+                            <h4>Gênero:</h4>
+                            <h5>${data.Genre}</h5>
+                        </div>
+                    </div>
+                </section>
+                <section id="modal-footer">
+                    <button id="add-to-list" onclick='{addToList(${JSON.stringify(
+                      data
+                    ).replace("'", "`")})}''>Adicionar à lista</button>
+                </section>
+            </section>`;
+}
+
+background.addEventListener("click", backgroundClickHandler);
